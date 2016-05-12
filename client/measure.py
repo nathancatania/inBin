c037b824ad7f98c4f074293ca7a9dd10a73714b7
Nathan Catania <nathancatania@users.noreply.github.com>
2016-05-12 13:23:06 +1000

    Added Client Source Files
    ultrasonic.cpp - Source file for program to obtain sensor measurement in millimeters.
    
    measure.py - Source file for client-side of system. Runs ultrasonic program to obtain measurement data, obtains public IP address and sends this data along with deviceID, latitude, longitude, and authorisation key.

---
+++ client/measure.py
@@ -0,0 +1,79 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+from subprocess import check_output
+
+# This is sent with Server payload.
+# Server will check string. If it does not match this exactly
+# then sent data will be discarded.
+# This is a quick/dirty way to prevent unauthorised POST data
+key = "xNdp9LbK9PSUTYRFDWk2Jr"
+deviceID = "melCBD,-37.809379,144.963595"
+
+#### Bin Co-Ordinates ####
+# Used for Google Maps API
+# must be separated by comma
+# -37.809379, 144.963595
+latitude = -37.809379
+longitude = 144.963595
+
+#TODO:
+# * Catch and generate timeout/retry for post
+# * Generate unique device IDs
+# * prefix device ID with name of device (stored in system?)
+
+# ERROR CODES
+# Error -2 == Issue with obtaining IP address.
+
+# Setup GPIO and Configure I/O
+def setup():
+    #### setup GPIO Pins ####
+    GPIO.setwarnings(false)
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM) # Use GPIO/BCM Numbering
+
+    #### Configure I/O ####
+    # Trig initial state must be LOW
+    GPIO.setup(TRIG, GPIO.OUT, initial = 0)
+    GPIO.setup(ECHO, GPIO.IN)
+    time.sleep(0.05) # delay 50ms to settle sensor
+    return
+
+# Obtain Distance from HC-SR04 Ultrasonic Sensor
+def getDistance():
+    # returns distance as a string
+    # calls ultrasonic executable
+    # removes newline \n char at end
+    distance = check_output(["sudo", "./ultrasonic"])
+    distance = distance[:-1]
+    print distance
+    return distance
+
+# Obtain current Public IP address (outside of router)
+def getPublicIP():
+    try:
+        # Any website that outputs nothing but
+        ##   the IP address in RAW text will work fine.
+        # http://icanhazip.com is another alt website to use.
+        # TODO: Code fallback website in case primary goes down.
+        ip = requests.get('http://ip.42.pl/raw')
+        ip = ip.text
+    except:
+        print "Error getting Public IP"
+        response = -2
+    else:
+        print ip
+        response = ip
+    finally:
+        return response
+
+#### MAIN ####
+# payload = {'key1': 'value1', 'key2': 'value2'}
+# r = requests.post("http://httpbin.org/post", data=payload)
+# print(r.text)
+# to getIP, just use 'origin' key.
+payload = {'distance':getDistance(), 'publicip':getPublicIP(), 'latitude':latitude, 'longitude':longitude, 'key':key, 'deviceID':deviceID}
+#r = requests.post("http://58.162.145.29/process.php", data=payload)
+r = requests.post("http://58.162.145.29/post.php", data=payload)
+print "Data Sent!"
+print(r.text)
---
+++ client/ultrasonic.cpp
@@ -0,0 +1,122 @@
+/*
+Ultrasonic Sensor HC-SR04
+Nathan Catania
+02/04/16
+For Design 3A/Telstra University Challenge, RMIT University
+
+v0.1
+(sudo nano ultrasonic.cpp)
+
+Interacts with HC-SR04 Ultrasonic sensor using GPIO Pins on RPi
+Sensor has 4 pins: Vcc, ECHO, TRIG, GND
+1. Send 5V/HIGH pulse for >10us to TRIG pin to start sensor
+2. Sensor sends 8x pulses at 40kHz and measures time to receive back
+3. Sensor outputs a 5V pulse on ECHO pin
+3a.	Period of pulse (in microseconds) is time taken for sensor to receive pulses back.
+3b.	Time taken should be /2 as it is time taken to send pulse to object and back.
+3c.	Speed = distance / (time/2)
+
+NB:
+	Speed == speed of sound (340m/s - depends on Temp - assume 15 degC avg)
+	Distance in cm = Time(us) / 58    (From Datasheet)
+	Distance in m  = [340 * [(t/2)/1000000]]
+	Distance in mm = [340 * [(t/2)/1000000]]*1000
+	/1000000 divisor converts the time t from microseconds to seconds
+
+WARNING:
+	RPi can SEND 5V signal, but CANNOT RECEIVE 5V SIGNAL (Max 3.3V)!
+	THIS WILL DESTROY THE RPI!
+	ECHO PIN WILL OUTPUT 5V TO RPI!
+	*MUST* USE VOLTAGE DIVIDER TO BRING VOLT OUTPUT OF ECHO TO 3.3V!
+	INPUT to RPi will be connected to Vout of Voltage Divider.
+	Use 10k and 4.7k resistors == approx 3.3 V for Vout.
+
+	VIN(ECHO PIN)------------
+				|
+				4.7k
+				|
+				----- VOUT to RPi
+				|
+				10k
+				|
+				----- GND on RPi
+
+PIN CONNECTIONS
+	Pin Connections are referenced using wiringPi naming
+	TRIG --> Pin 5 (GPIO/BCM Pin 24, Physical Pin 18)
+	ECHO --> Pin 6 (GPIO/BCM Pin 25, Physical Pin 22)
+	GND  --> Physical Pin 18 on RPi
+	Vcc  --> Physical Pin 2 or 4 on RPi (both are +5V)
+
+Based on: https://nineof.wordpress.com/2013/07/16/rpi-hc-sr04-ultrasonic-sensor-mini-project/
+*/
+
+#include <iostream>
+#include <wiringPi.h>
+
+// define wiringPi Pins
+// change these depending on pins used
+#define TRIG 5
+#define ECHO 6
+
+void setup() {
+	wiringPiSetup();	// required to use wiringPi library
+
+	// Setup GPIO Pins for Input/Output
+	// TRIG is OUTPUT (From Pi to Sensor)
+	// ECHO is INPUT (From Sensor to Volt Divider to Pi)
+	pinMode(TRIG, OUTPUT);
+	pinMode(ECHO, INPUT);
+
+	// TRIG pin initial state must be LOW
+	digitalWrite(TRIG, LOW);
+	delay(30);	// delay in milliseconds (ms) - allow to settle
+	return;
+}
+
+// Distane in mm
+double getDistance() {
+	// Send TRIG pulse to start sensor
+	// Hold HIGH for >10us --> Using 20us to be safe
+	// WARNING POTENTIAL SYSTEM IMPACT: delayMicroseconds() uses infinite loop that continuously
+	//	polls system time until complete. Delays <100us will do this.
+	// After elapsed time, return TRIG to LOW.
+	digitalWrite(TRIG, HIGH);
+	delayMicroseconds(20);		// delay in microseconds (us)
+	digitalWrite(TRIG, LOW);
+
+	// define variables to store time values
+	// time values sent by sensor are integers
+	long timeStart = 0;
+	long timeFinish = 0;
+
+	// Wait for sensor to return pulse
+	//std::cout << "Waiting for sensor data...\n";
+	while(digitalRead(ECHO) == LOW);
+	timeStart = micros();	// will save the time (microseconds) of the instant ECHO goes HIGH
+
+	// Wait for ECHO pulse to end. Record H/L transition as finish time.
+	while(digitalRead(ECHO) == HIGH);
+	timeFinish = micros();
+	//std::cout << "[ OK! ] Data Received!\n";
+
+	// calculate period of ECHO
+	long travelTime = timeFinish - timeStart;
+	//std::cout << "ECHO Pulse Period = " << travelTime << " us.\n";
+
+	// calculate distance in mm
+	//double distance = 0.17*travelTime; //also works.
+	double distance = (340.0 * ((travelTime/2)/1000000.0))*1000.0;
+
+	return distance;
+}
+
+int main() {
+
+	setup();
+
+	delay(1000); // delay 1s. Running program quickly generates inaccurate measurements.
+	std::cout << getDistance() << std::endl;
+
+	return 0;
+}
