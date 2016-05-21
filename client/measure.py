260f6311a752646513e34ec26ea6e9ecb045f787
Nathan Catania <nathancatania@users.noreply.github.com>
2016-05-21 14:01:07 +1000

    Now calculates fill level and sends to server

--- client/measure.py
+++ client/measure.py
@@ -5,14 +5,26 @@
 # For Team inBin. Telstra University Challenge 2016
 # s3477902@student.rmit.edu.au, RMIT University
 
-# v0.7
-# 09/05/16
+# v0.8
+# Last updated: 17/05/16
+# Changelog is at bottom of file.
 
 import RPi.GPIO as GPIO
 import time
 import requests
 from subprocess import check_output
 
+#### Bin DEPTH ####
+# [LARGE] 240 L Capacity
+# Define DEPTH in mm (millimeters):
+capacity = 950.0
+
+#### Trigger Level ####
+# Fill level of bin that triggers need to empty
+# As a percentage, eg: 60%, 75%, 80% etc...
+limit = 0.75
+
+#### Authentication and ID ####
 # This is sent with Server payload.
 # Server will check string. If it does not match this exactly
 # then sent data will be discarded.
@@ -24,8 +36,8 @@ deviceID = "demo,0001"
 # Used for Google Maps API
 # must be separated by comma
 # -37.809379, 144.963595
-latitude = -37.809379
-longitude = 144.963595
+latitude = -37.918961
+longitude = 145.239983
 
 #TODO:
 # * Catch and generate timeout/retry for post
@@ -77,13 +89,36 @@ def getPublicIP():
     finally:
         return response
 
+# From getDistance() calculate fill level of bin as percentage
+# empty is bool whether bin needs emptying or not
+distance = getDistance()
+distance = float(distance)
+print distance
+
+fill = 1 - (distance/capacity)
+print fill
+
+if(fill > limit):
+    empty = 1
+else:
+    empty = 0
+
+print empty
+
 #### MAIN ####
 # payload = {'key1': 'value1', 'key2': 'value2'}
 # r = requests.post("http://httpbin.org/post", data=payload)
 # print(r.text)
 # to getIP, just use 'origin' key.
-payload = {'distance':getDistance(), 'publicip':getPublicIP(), 'latitude':latitude, 'longitude':longitude, 'key':key, 'deviceID':deviceID}
+payload = {'distance':distance, 'fill':fill, 'capacity':capacity, 'empty':empty, 'publicip':getPublicIP(), 'latitude':latitude, 'longitude':longitude, 'key':key, 'deviceID':deviceID}
 #r = requests.post("http://58.162.145.29/process.php", data=payload)
 r = requests.post("http://58.162.145.29/post.php", data=payload)
 print "Data Sent!"
 print(r.text)
+
+
+##### CHANGELOG #####
+# v0.7 - [11/05/16] -   Program finished. Sending data via POST successfully
+
+# v0.8 - [17/05/16] -   Updated Location co-ordinates to reflect demo location (ROWVILLE, VIC)
+#                   -   Added calculations for bin fill level + capacity + whether it requires emptying.
